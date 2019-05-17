45fc46af6d8cf22d44e53526bf4b9e1ba32b8768
Mark Hernandez <m.hern1415@gmail.com>
2019-05-17 19:31:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,10 +10,10 @@ class EmailParser
     @email = emails
   end
   
-  
-  
-  
-  
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
 end
-  
-  
